247672e9169af82bdf0e1bc7663628524ce8cc6e
ethangardner <ethan.gardner@gsa.gov>
2024-11-22 16:10:04 -0500

    Integrate web-vitals with sendToAnalytics
    Refactor web-vitals imports and remove onFCP. Add conditional checks to set up analytics event listeners and integrate CLS, INP, and LCP metrics with sendToAnalytics, ensuring they report only when 'gas4' is defined on the window or after the specific event is fired.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import { onCLS, onFCP, onINP, onLCP } from './web-vitals.js';
+import { onCLS, onINP, onLCP } from './web-vitals.js';
+import { sendToAnalytics } from './send-to-analytics.js';
 
-export { onCLS, onFCP, onINP, onLCP };
+if(typeof window !== 'undefined' && 'gas4' in window) {
+  console.log('already defined');
+  onCLS(sendToAnalytics);
+  onINP(sendToAnalytics);
+  onLCP(sendToAnalytics);
+} else {
+  window.addEventListener('dap-universal-federated-analytics-load', () => {
+    console.log('listener fired');
+    onCLS(sendToAnalytics);
+    onINP(sendToAnalytics);
+    onLCP(sendToAnalytics);
+  });
+}
