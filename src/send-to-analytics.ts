4dab1e8648cd222823da7065613740b51006e39f
ethangardner <ethan.gardner@gsa.gov>
2024-11-22 16:09:26 -0500

    Add sendToAnalytics function for web vitals reporting
    This change introduces a new function `sendToAnalytics` to the codebase, which formats and sends web vitals data to Google Analytics. This includes essential metric parameters and custom parameters for detailed querying and aggregation within BigQuery.

---
+++ src/send-to-analytics.ts
@@ -0,0 +1,30 @@
+import { formatEventData, type WebVitalsAttribution, type WebVitalsName } from './format-event-data.js';
+
+declare const gas4: any;
+
+export const sendToAnalytics = ({
+  name,
+  delta,
+  value,
+  id,
+  attribution,
+}: {
+  name: string;
+  delta: number;
+  value: number;
+  id: string;
+  attribution: WebVitalsAttribution;
+}) => {
+  if (typeof gas4 === 'function') {
+    gas4(name, {
+      // Built-in params:
+      value: delta, // Use `delta` so the value can be summed.
+      // Custom params:
+      metric_id: id, // Needed to aggregate events.
+      metric_value: value, // Value for querying in BQ
+      metric_delta: delta, // Delta for querying in BQ
+      // Send the returned values from getDebugInfo() as custom parameters
+      ...formatEventData((name as WebVitalsName), attribution),
+    });
+  }
+};
