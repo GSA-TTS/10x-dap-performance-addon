567a3548412ea12afcb5dc30a81c9b435d7b9942
Ethan Gardner <edgincvg@gmail.com>
2024-11-22 00:23:38 -0500

    Basic test to make sure exports are provided.

--- test/dap-performance-addon.test.ts
+++ test/dap-performance-addon.test.ts
@@ -1,32 +1,11 @@
-import { html } from 'lit';
-import { fixture, expect } from '@open-wc/testing';
-import { DapPerformanceAddon } from '../src/DapPerformanceAddon.js';
-import '../src/dap-performance-addon.js';
+import { expect } from '@open-wc/testing';
+import { onCLS, onFCP, onINP, onLCP } from '../src/index.js';
 
-describe('DapPerformanceAddon', () => {
-  it('has a default header "Hey there" and counter 5', async () => {
-    const el = await fixture<DapPerformanceAddon>(html`<dap-performance-addon></dap-performance-addon>`);
-
-    expect(el.header).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
-  });
-
-  it('increases the counter on button click', async () => {
-    const el = await fixture<DapPerformanceAddon>(html`<dap-performance-addon></dap-performance-addon>`);
-    el.shadowRoot!.querySelector('button')!.click();
-
-    expect(el.counter).to.equal(6);
-  });
-
-  it('can override the header via attribute', async () => {
-    const el = await fixture<DapPerformanceAddon>(html`<dap-performance-addon header="attribute header"></dap-performance-addon>`);
-
-    expect(el.header).to.equal('attribute header');
-  });
-
-  it('passes the a11y audit', async () => {
-    const el = await fixture<DapPerformanceAddon>(html`<dap-performance-addon></dap-performance-addon>`);
-
-    await expect(el).shadowDom.to.be.accessible();
+describe('DAP Performance Addon', () => {
+  it('defines methods to track Core Web Vitals', () => {
+    expect(typeof onCLS).to.be('function');
+    expect(typeof onFCP).to.be('function');
+    expect(typeof onINP).to.be('function');
+    expect(typeof onLCP).to.be('function');
   });
 });
