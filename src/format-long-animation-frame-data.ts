650a71a628c465b81cd5b97d548fc67dcd71d907
Ethan Gardner <edgincvg@gmail.com>
2024-11-28 02:27:21 -0500

    fix linting issue

--- src/format-long-animation-frame-data.ts
+++ src/format-long-animation-frame-data.ts
@@ -28,17 +28,17 @@ import type { INPAttribution } from 'web-vitals';
  */
 export const formatLongAnimationFrameData = (attribution: INPAttribution) => {
   const loafEntries = attribution.longAnimationFrameEntries;
-  if(typeof loafEntries === 'undefined' || loafEntries.length === 0) {
+  if (typeof loafEntries === 'undefined' || loafEntries.length === 0) {
     return {};
   }
 
   let loafAttribution = {
-    debug_loaf_script_total_duration: 0
+    debug_loaf_script_total_duration: 0,
   };
 
   // The last LoAF entry is usually the most relevant.
   const loaf = attribution.longAnimationFrameEntries.at(-1);
-  if(typeof loaf !== 'undefined') {
+  if (typeof loaf !== 'undefined') {
     const loafEndTime = loaf.startTime + loaf.duration;
     loaf?.scripts.forEach(script => {
       if (script.duration <= loafAttribution.debug_loaf_script_total_duration) {
@@ -48,17 +48,32 @@ export const formatLongAnimationFrameData = (attribution: INPAttribution) => {
         // Stats for the LoAF entry itself.
         debug_loaf_entry_start_time: loaf.startTime,
         debug_loaf_entry_end_time: loafEndTime,
-        debug_loaf_entry_work_duration: loaf.renderStart ? loaf.renderStart - loaf.startTime : loaf.duration,
-        debug_loaf_entry_render_duration: loaf.renderStart ? loafEndTime - loaf.renderStart : 0,
-        debug_loaf_entry_total_forced_style_and_layout_duration: loaf.scripts.reduce((sum, script) => sum + script.forcedStyleAndLayoutDuration, 0),
-        debug_loaf_entry_pre_layout_duration: loaf.styleAndLayoutStart ? loaf.styleAndLayoutStart - loaf.renderStart : 0,
-        debug_loaf_entry_style_and_layout_duration: loaf.styleAndLayoutStart ? loafEndTime - loaf.styleAndLayoutStart : 0,
+        debug_loaf_entry_work_duration: loaf.renderStart
+          ? loaf.renderStart - loaf.startTime
+          : loaf.duration,
+        debug_loaf_entry_render_duration: loaf.renderStart
+          ? loafEndTime - loaf.renderStart
+          : 0,
+        debug_loaf_entry_total_forced_style_and_layout_duration:
+          loaf.scripts.reduce(
+            (sum, entry) => sum + entry.forcedStyleAndLayoutDuration,
+            0,
+          ),
+        debug_loaf_entry_pre_layout_duration: loaf.styleAndLayoutStart
+          ? loaf.styleAndLayoutStart - loaf.renderStart
+          : 0,
+        debug_loaf_entry_style_and_layout_duration: loaf.styleAndLayoutStart
+          ? loafEndTime - loaf.styleAndLayoutStart
+          : 0,
 
         // Stats for the longest script in the LoAF entry.
         debug_loaf_script_total_duration: script.duration,
-        debug_loaf_script_compile_duration: script.executionStart - script.startTime,
-        debug_loaf_script_exec_duration: script.startTime + script.duration - script.executionStart,
-        debug_loaf_script_forced_style_and_layout_duration: script.forcedStyleAndLayoutDuration,
+        debug_loaf_script_compile_duration:
+          script.executionStart - script.startTime,
+        debug_loaf_script_exec_duration:
+          script.startTime + script.duration - script.executionStart,
+        debug_loaf_script_forced_style_and_layout_duration:
+          script.forcedStyleAndLayoutDuration,
         debug_loaf_script_type: script.invokerType,
         debug_loaf_script_invoker: script.invoker,
         debug_loaf_script_source_url: script.sourceURL,
@@ -69,7 +84,7 @@ export const formatLongAnimationFrameData = (attribution: INPAttribution) => {
 
         // LoAF metadata.
         debug_loaf_meta_length: loafEntries.length,
-      }
+      };
     });
   }
 
@@ -78,8 +93,10 @@ export const formatLongAnimationFrameData = (attribution: INPAttribution) => {
   }
 
   // The LoAF script with the single longest total duration.
-  return Object.fromEntries(Object.entries(loafAttribution).map(([k, v]) =>
-    // Convert all floats to ints.
-     [k, typeof v === 'number' ? Math.floor(v) : v]
-  ));
-}
+  return Object.fromEntries(
+    Object.entries(loafAttribution).map(([k, v]) =>
+      // Convert all floats to ints.
+      [k, typeof v === 'number' ? Math.floor(v) : v],
+    ),
+  );
+};
